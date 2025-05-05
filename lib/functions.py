9246d5cbf15d63e504b1643f874534f7542c7bed
Chadwick Koo <chadwickkoo@outlook.com>
2025-05-05 15:35:42 +0300

    functions.py file

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
 def greet(name):
+    print(f"Hello, {name}!")
     pass
+
+greet("Naureen")
+
+
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
     pass
+
+greet_with_default()
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+add(45,55)
+
 
 def halve(number):
-    pass
+    return number / 2
+
+halve(100)
